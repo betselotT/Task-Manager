1b8bf8112f26a6b1dc18d5c3576de6db5782e0e9
betselotT <betselottesfa9@gmail.com>
2025-05-04 05:34:25 -0900

    implemented auth and storage

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,19 @@
 type FormType = "sign-in" | "sign-up";
+
+interface SignUpParams {
+  uid: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface SignInParams {
+  email: string;
+  idToken: string;
+}
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+}
